dfe5d41383cb6aa6c87e241be65c1ca79ceb08cb
JoonaLuttinen <joona.luttinen@edu.turkuamk.fi>
2023-10-05 16:46:48 +0300

    project unicafe aloitettu

---
+++ Osa_1/unicafe/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+
+
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      code here
+    </div>
+  )
+}
+
+export default App
