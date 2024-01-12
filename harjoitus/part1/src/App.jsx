e6afa8067f82f4101c7d4611f75ff5b6d3afb7e5
JoonaLuttinen <joona.luttinen@edu.turkuamk.fi>
2024-01-12 13:50:19 +0200

    part 2 continues

--- harjoitus/part1/src/App.jsx
+++ harjoitus/part1/src/App.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 const App = (props) => {
   const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState()
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true) 
 
   const hook = () => {
@@ -23,22 +23,37 @@ const App = (props) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      important: Math.random() > 0.5,
-      id: notes.length + 1,
+      important: Math.random() > 0.5
     }
   
-    setNotes(notes.concat(noteObject))
+    axios
+    .post('http://localhost:3001/notes', noteObject)
+    .then(response => {
+      setNotes(notes.concat(response.data))
     setNewNote('')
+    })
+
+    
   }
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
   const notesToShow = showAll
   ? notes
   : notes.filter(note => note.important === true)
+
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+  }
+
 
   return (
     <div>
@@ -50,10 +65,10 @@ const App = (props) => {
       </div>
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
         )}
       </ul>
-      <form onSubmit={addNote}>
+      <form onSubmit={addNote} name="notes">
         <input value={newNote} onChange={handleNoteChange}/>
         <button type="submit">save</button>
       </form>   
