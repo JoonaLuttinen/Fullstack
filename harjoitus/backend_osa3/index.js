b704c1a04b16a8852faea6c92dda0e7702e537de
JoonaLuttinen <joona.luttinen@edu.turkuamk.fi>
2024-08-07 17:33:31 +0300

    modifications

--- harjoitus/backend_osa3/index.js
+++ harjoitus/backend_osa3/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.use(cors())
 app.use(express.static('dist'))
+app.use(cors())
 app.use(express.json())
 
 
@@ -95,7 +95,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
